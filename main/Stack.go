9c1a6bb62e1dc5cbf48ac2d77baee115654054cc
M Shakhawat Hossain safat <shakhawat.hossain@therapservices.net>
2018-03-05 18:44:48 +0600

    Implemented Stack DS

---
+++ main/Stack.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type Element struct {
+	value interface{}
+	next  *Element
+}
+
+type Stack struct {
+	size int
+	top  *Element
+}
+
+func (stack *Stack) Push(value interface{}) {
+	stack.top = &Element{value, stack.top}
+	stack.size++
+}
+
+func (stack *Stack) Pop() (value interface{}) {
+	if stack.size > 0 {
+		value, stack.top = stack.top, stack.top.next
+		stack.size--
+
+		return
+	}
+
+	return nil
+}
+
+func main() {
+	var stack = Stack{}
+
+	stack.Push("java")
+	stack.Push("php")
+	stack.Push("c")
+	stack.Push("cpp")
+	stack.Push("go")
+
+	fmt.Println(stack.Pop(), stack.size)
+	fmt.Println(stack.Pop())
+	fmt.Println(stack.Pop())
+	fmt.Println(stack.Pop())
+	fmt.Println(stack.Pop())
+}
